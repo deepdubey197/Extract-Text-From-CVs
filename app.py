ad8213652ce3697ee3db795f35fc8daad0bdb4b6
Deep Dubey <117110243+deepdubey197@users.noreply.github.com>
2024-04-15 17:16:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ def extract_text_from_docx(file_path):
 
 def extract_text_from_pdf(file_path):
     with open(file_path, 'rb') as file:
-        reader = PdfReader(file)
+        reader = PdfFileReader(file)
         text = ''
-        for page_num in range(len(reader.pages)):
-            text += reader.pages[page_num].extract_text()
+        for page_num in range(reader.numPages):
+            text += reader.getPage(page_num).extractText()
     return text
 
 def extract_info(text):
